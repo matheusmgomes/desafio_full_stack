08e911731bb5045d70ae2696e47a444aeaa3dc7a
Matheus Machado <machado_gomes@hotmail.com>
2026-04-22 10:58:26 -0300

    api backend

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const users = []
+
+app.post("/users", (req, res) => {
+  const { name, email } = req.body
+
+  if (!name || !email) {
+    return res.status(400).json({ message: "Campos obrigatórios não foram preenchidos." })
+  }
+
+  if (users.find(user => user.email.toLowerCase() === email.toLowerCase())) {
+    return res.status(409).json({ message: "E-mail já cadastrado." })
+  }
+
+  const user = {
+    name,
+    email
+  }
+
+  users.push(user)
+
+  res.status(201).json({
+    message: 'Usuário cadastrado com sucesso!',
+    usuario: user
+  });
+})
+
+app.get("/users", (req, res) => {
+  res.status(200).json(users)
+})
+
+app.listen(3000, () => {
+  console.log("server up")
+})
